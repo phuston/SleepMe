98cf6231a81eca7d8d1b65612b3122e01e9aa215
Patrick Huston <patrickhuston1@gmail.com>
2015-03-20 00:26:42 -0400

    Adds wakeup list architecture.

---
+++ app/src/main/java/com/phuston/android/sleepme/WakeList.java
@@ -0,0 +1,39 @@
+package com.phuston.android.sleepme;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+/**
+ * Created by phuston on 3/14/15.
+ */
+public class WakeList {
+    private Calendar mInitTime = Calendar.getInstance();
+
+    public WakeList(int ListType, Calendar InitTime) {
+        mInitTime = InitTime;
+    }
+
+    public ArrayList<Calendar> mTimeList;
+
+
+    public WakeList(Calendar initTime){
+        mInitTime = initTime;
+        mInitTime.add(Calendar.MINUTE, 14);
+    }
+
+    public ArrayList<Calendar> getTimes(){
+
+        ArrayList<Calendar> times = new ArrayList<Calendar>();
+        times.add(mInitTime);
+
+        for(int i = 0; i < 10; i++){
+            mInitTime.add(Calendar.HOUR, 1);
+            mInitTime.add(Calendar.MINUTE, 30);
+            times.add(mInitTime);
+        }
+
+        return times;
+    }
+
+
+}

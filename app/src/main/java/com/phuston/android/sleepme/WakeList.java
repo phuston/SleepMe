280945daf2b1403c765725110ec2a26c372b0cd1
Patrick Huston <patrickhuston1@gmail.com>
2015-03-22 00:56:36 -0400

    Fixes messed up time issues.

--- app/src/main/java/com/phuston/android/sleepme/WakeList.java
+++ app/src/main/java/com/phuston/android/sleepme/WakeList.java
@@ -20,10 +20,13 @@ public class WakeList {
     public ArrayList<Calendar> getTimes(){
 
         ArrayList<Calendar> times = new ArrayList<>();
-        times.add(mInitTime);
 
         for(int i = 1; i < 8; i++){
-            Calendar newTime = mInitTime;
+            Calendar newTime = Calendar.getInstance();
+            newTime.set(Calendar.DATE, mInitTime.DATE);
+            newTime.set(Calendar.DAY_OF_WEEK, mInitTime.DAY_OF_WEEK);
+            newTime.set(Calendar.HOUR, mInitTime.HOUR);
+            newTime.set(Calendar.MINUTE, mInitTime.MINUTE);
             newTime.add(Calendar.HOUR, i);
             newTime.add(Calendar.MINUTE, i*30);
             times.add(newTime);
